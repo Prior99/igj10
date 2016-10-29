e356bacdeaddf1d6e936a6ff36a1716c9961ffd6
Shi Gotoh <shigotoh1@gmail.com>
2016-10-29 16:08:07 +0200

    scale sidewalk length with maplength

--- src/systems/map.hpp
+++ src/systems/map.hpp
@@ -23,23 +23,26 @@ class MapSystem : public entityx::System<MapSystem> {
 			srand (15);
         //calculate values
         int housewidth = 128;
+        float housemargin = 250.f;
         int sidewalkwidth = 64;
+        int maplength = 10;
 
         // create houses
-		    for (int i = 0; i < 10; i++) {
+		    for (int i = 0; i < maplength * floor(housewidth/housewidth); i++) {
             float height = 200.f + rand() % 100 - 50;
 		        entityx::Entity box1 = es.create();
-		        box1.assign<Position>(glm::vec2(i * 250.f, height));
+		        box1.assign<Position>(glm::vec2(i * housemargin, height));
 		        box1.assign<Box>(glm::vec2(128.f, 128.f));
 		        box1.assign<Drawable>("house", housewidth, 128);
+            std::cout << i << std::endl;
 		    }
-
         // create sidewalk
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < maplength * (floor(housewidth/sidewalkwidth)+floor(housemargin/sidewalkwidth)); i++) {
 	        entityx::Entity sidewalk = es.create();
 	        sidewalk.assign<Position>(glm::vec2(0.f + i * sidewalkwidth, 400));
 	        sidewalk.assign<Box>(glm::vec2(64.f, 48.f));
 	        sidewalk.assign<Drawable>("sidewalk", sidewalkwidth, 48);
+          std::cout << i << std::endl;
         }
 			created = true;
 		}
